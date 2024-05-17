84d73d5d66e19f589c442d5947bd8f5f7199131f
tobias.tlaskal <tobias.tlaskal@getready.cz>
2024-05-17 20:05:39 +0200

    Projekt vytvořen

---
+++ src/main/java/tlaskal/tobias/knihovnawww/KnihovnaWwwApplication.java
@@ -0,0 +1,13 @@
+package tlaskal.tobias.knihovnawww;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnihovnaWwwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnihovnaWwwApplication.class, args);
+	}
+
+}
